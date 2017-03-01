775c673c2ba84e52c47650ef313c6dce9ae330b8
Attesa <calstream.lgn@gmail.com>
2017-03-01 23:54:17 +0300

    👉

--- Flow/flow.cpp
+++ Flow/flow.cpp
@@ -1,5 +1,6 @@
 #include<fstream>
 #include<vector>
+#include<climits>
 
 using namespace std;
 
@@ -8,11 +9,59 @@ string const oname = "output.txt";
 
 class Graph
 {
+private:
+	struct edge
+	{
+		int to;
+		int from;
+		int flow;
+		int cap;
+		int rev; // rev --
+	};
+
+	struct mage
+	{
+		int req_e;
+		bool dg;
+		int n;
+	};
+
+	int mages_n; // number of mages - G
+	vector<int> levels;
+	vector<mage> mages;
+	vector<edge> adj; // 
+
 public:
 	Graph()
 	{
 		ifstream input;
 		input.open(iname);
+		input >> mages_n;
+		for (int i = 0; i < mages_n; i++)
+		{
+			mage m;
+			input >> m.req_e;
+			input >> m.dg;
+			input >> m.n;
+			mages.push_back(m);
+		}
+
+		for (int i = 0; i < mages_n; i++)
+		{
+			for (int j = 0; j < mages[i].n; j++)
+			{
+				int tt, tc;
+				input >> tt;
+				input >> tc;
+				adj.push_back({tt, i, -1, tc, -1});
+			}
+			if (mages[i].dg)
+			{
+				adj.push_back({ mages_n, i, -1, INT_MAX, -1 });
+			}
+		}
+
+		input.close();
 
 	}
 
@@ -21,6 +70,7 @@ public:
 		ofstream output;
 		output.open(oname);
 		output.clear();
+		output.close();
 	}
 };
 
